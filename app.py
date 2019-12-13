01bd4cccd42da747a056150d5934694b59e70836
300931676 <300931676@PR-A313-01>
2019-12-13 10:03:11 -0500

    Added selectbox for hour and status to generate map

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Visualization packages
 import matplotlib.pyplot as plt
 
-DATA_URL="C:/Users/iamgu/Centennial/Semester 6/Data Warehouse/Project/dataset/Bicycle_Thefts.csv"
+DATA_URL="C:/Users/300931676/DataAnalysisUsingStreamlit/dataset/Bicycle_Thefts.csv"
 
 # @st.cache(suppress_st_warning=True)
 
@@ -19,18 +19,24 @@ def load_data():
 
 def main():
     # Assigning title and subheader
-    st.title("COMP309 Project")
     # Get data from load_data method 
     data=load_data()
     st.subheader("Raw Data")
     # Writing data to streamlit
     if st.checkbox('View Data'):
         st.write(data)
-    st.subheader("Map of bikes lost in toronto")
+    st.subheader("Map of lost bikes")
     if st.checkbox('View Map'):
-        st.map(data)
+        status=st.selectbox('Select status',data.status.unique())
+        hour=st.selectbox('Select hour',data.occurrence_time.unique())
+        data_status=data[data.status==status]
+        data_map=data_status[data.occurrence_time==hour]
+        st.map(data_map)
+        total_count=data_map.status.count()
+        st.text('Total count: %s' %  total_count)
     if st.checkbox('Visualize'):
         visualize_data(data)
+    
 
     
 
@@ -44,6 +50,9 @@ def visualize_data(data):
     if st.checkbox('Visualize bike type'):
         stolen_bike_type=data.loc[data.status=='STOLEN']['bike_type']
         st.text('Total count of stolen bikes by type: %s'% stolen_bike_type.count())
-        st.bar_chart(stolen_bike_type.value_counts())  
+        st.bar_chart(stolen_bike_type.value_counts()) 
+        
+
+    
 if __name__== '__main__':
     main()
